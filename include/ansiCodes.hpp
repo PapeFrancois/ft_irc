53b9a3c17fd9e98ce6981081147047f7fa4d46bb
Heloise POMPIDOU <hepompid@paul-f4Br7s4.clusters.42paris.fr>
2025-04-24 10:01:24 +0200

    ajout du header ansiCodes pr alleger

---
+++ include/ansiCodes.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ansiCodes.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/24 09:48:38 by hepompid          #+#    #+#             */
+/*   Updated: 2025/04/24 09:48:54 by hepompid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef ANSI_CODES
+#  define ANSI_CODES
+
+#  define RESET "\033[0m"
+#  define BOLD "\033[1m"
+#  define UNDERLINE "\033[4m"
+
+#  define BLACK "\033[30m"
+#  define RED "\033[31m"
+#  define GREEN "\033[32m"
+#  define YELLOW "\033[33m"
+#  define BLUE "\033[34m"
+#  define PURPLE "\033[35m"
+#  define CYAN "\033[36m"
+#  define WHITE "\033[37m"
+
+#  define BLACK_BG "\033[40m"
+#  define RED_BG "\033[41m"
+#  define GREEN_BG "\033[42m"
+#  define YELLOW_BG "\033[43m"
+#  define BLUE_BG "\033[44m"
+#  define PURPLE_BG "\033[45m"
+#  define CYAN_BG "\033[46m"
+#  define WHITE_BG "\033[47m"
+# endif
--- include/irc.hpp
+++ include/irc.hpp
@@ -6,7 +6,7 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 17:37:30 by hepompid          #+#    #+#             */
-/*   Updated: 2024/11/21 11:49:53 by hepompid         ###   ########.fr       */
+/*   Updated: 2025/04/24 09:49:27 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,32 +18,8 @@
 # include <cctype>
 
 # include "Server.hpp"
+# include "ansiCodes.hpp"
 
-# ifndef ANSI_CODES
-#  define ANSI_CODES
-
-#  define RESET "\033[0m"
-#  define BOLD "\033[1m"
-#  define UNDERLINE "\033[4m"
-
-#  define BLACK "\033[30m"
-#  define RED "\033[31m"
-#  define GREEN "\033[32m"
-#  define YELLOW "\033[33m"
-#  define BLUE "\033[34m"
-#  define PURPLE "\033[35m"
-#  define CYAN "\033[36m"
-#  define WHITE "\033[37m"
-
-#  define BLACK_BG "\033[40m"
-#  define RED_BG "\033[41m"
-#  define GREEN_BG "\033[42m"
-#  define YELLOW_BG "\033[43m"
-#  define BLUE_BG "\033[44m"
-#  define PURPLE_BG "\033[45m"
-#  define CYAN_BG "\033[46m"
-#  define WHITE_BG "\033[47m"
-# endif
 
 class NotNumber : public std::exception
 {
