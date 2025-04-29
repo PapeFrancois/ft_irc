2161249ecd6fa4e4bdc80b073eaee0d0597c039e
Heloise POMPIDOU <hepompid@student.42.fr>
2025-04-29 17:09:12 +0200

    modif des macro reply, echange du nom de param nick pour client, plus clair

--- include/reply.hpp
+++ include/reply.hpp
@@ -6,7 +6,7 @@
 /*   By: hepompid <hepompid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 20:44:43 by hepompid          #+#    #+#             */
-/*   Updated: 2024/12/10 20:39:53 by hepompid         ###   ########.fr       */
+/*   Updated: 2025/04/29 17:08:31 by hepompid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 
 typedef std::string str;
 
-# define RPL_WELCOME(nick) ("001 " + str(nick) + " :Salut ma couille\r\n")
-# define RPL_YOURHOST(nick, serverName, version) ("002 " + str(nick) + " :Your host is " + str(serverName) + " running in version " + str(version) + "\r\n")
-# define RPL_CREATED(nick, dateTime) ("003 " + str(nick) + " :This server was created " + str(dateTime) + "\r\n")
-# define RPL_MYINFO(nick, serverName, version) ("004 " + str(nick) + " " + str(serverName) + " " + str(version) + "\r\n")
-# define ERR_NONICKNAMEGIVEN(nick) ("431 " + str(nick) + " :No nickname given\r\n")
-# define ERR_ERRONEUSNICKNAME(nick, wantedNick) ("432 " + str(nick) + str(wantedNick) + ":Erroneus nickname\r\n")
-# define ERR_NICKNAMEINUSE(nick, wantedNick) ("433 " + str(nick) + str(wantedNick) + " :Nickname is already in use\r\n")
-# define ERR_NEEDMOREPARAMS(nick, command) ("461 " + str(nick) + " " + str(command) + " :Not enough parameters\r\n")
-# define ERR_ALREADYREGISTERED(nick) ("462 " + str(nick) + " :You may not reregister\r\n")
-# define ERR_PASSWDMISMATCH(nick) ("464 " + str(nick) + " :Password incorrect\r\n")
+# define RPL_WELCOME(client) ("001 " + str(client) + " :Salut ma couille\r\n")
+# define RPL_YOURHOST(client, serverName, version) ("002 " + str(client) + " :Your host is " + str(serverName) + " running in version " + str(version) + "\r\n")
+# define RPL_CREATED(client, dateTime) ("003 " + str(client) + " :This server was created " + str(dateTime) + "\r\n")
+# define RPL_MYINFO(client, serverName, version) ("004 " + str(client) + " :" + str(serverName) + " " + str(version) + "\r\n")
+# define ERR_NOSUCHNICK(client, target) ("401 " + str(client) + " " + str(target) + " :No such nick\r\n")
+# define ERR_CANNOTSENDTOCHAN(client, channel) ("404 " + str(client) + " " + str(channel) + " :Cannot send to channel\r\n")
+# define ERR_NOTEXTTOSEND(client) ("412 " + str(client) + " :No text to send\r\n")
+# define ERR_NONICKNAMEGIVEN(client) ("431 " + str(client) + " :No nickname given\r\n")
+# define ERR_ERRONEUSNICKNAME(client, nickname) ("432 " + str(client) + " " + str(nickname) + ":Erroneus nickname\r\n")
+# define ERR_NICKNAMEINUSE(client, nickname) ("433 " + str(client) + " " + str(nickname) + " :Nickname is already in use\r\n")
+# define ERR_NEEDMOREPARAMS(client, command) ("461 " + str(client) + " " + str(command) + " :Not enough parameters\r\n")
+# define ERR_ALREADYREGISTERED(client) ("462 " + str(client) + " :You may not reregister\r\n")
+# define ERR_PASSWDMISMATCH(client) ("464 " + str(client) + " :Password incorrect\r\n")
+
+# define QUIT_MSG ":ERROR Connection ended from client\r\n"
+// # define NICK_CHANGE(oldNick, newNick) (":" + str(oldNick) + "!hepompid@127.0.0.1 " + " NICK :" + str(newNick) + "\r\n")
 
 #endif
